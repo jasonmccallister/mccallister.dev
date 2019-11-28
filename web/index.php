933f16cef1c67b88e8cd5c450ef6ac8cdfc82f80
Jason McCallister <themccallister@gmail.com>
2019-11-28 11:20:47 -0500

    need to add debug to use env var

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 
-// comment out the following two lines when deployed to production
+// TODO comment out the following two lines when deployed to production
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
